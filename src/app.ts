755b6d08667006093925ea56162d1614f798f05f
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-04-04 16:04:05 -0300

    chore: starting to setting prisma orm

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 // Create a Fastify server
 export const app = fastify();
+
+// create an instance of PrismaClient to access the database
+const prisma = new PrismaClient();
