a71a11616929b4553aba3bb2a23a0e6377353756
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-04-06 10:43:36 -0300

    chore: vitest

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// in this way, vitest will understand the tsconfig paths, like "@/*"
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+});
