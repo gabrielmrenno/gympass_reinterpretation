452c35842b88f29e0404ff05fef0fe276628f983
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-04-06 10:35:43 -0300

    add: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { prisma } from "./lib/prisma";
-import { register } from "./http/controllers/register";
+import { ZodError, z } from "zod";
 import { appRoutes } from "./http/routes";
+import { env } from "./env";
 
 // Create a Fastify server
 export const app = fastify();
 
 // register the routes' plugin
 app.register(appRoutes);
+
+// creating a global error handler
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: log error to a service like Sentry, Datadog, etc
+  }
+
+  return reply.status(500).send({
+    message: "Internal server error",
+  });
+});
