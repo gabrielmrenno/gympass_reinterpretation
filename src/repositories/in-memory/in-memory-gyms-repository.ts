99fead61dbffc5d6c67c84dbc497f5994777b10b
gabrielrennoprime <grenno@ext.grupo-artico.com>
2023-04-11 10:01:34 -0300

    feat: search nearby gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
-import { Gym, Prisma, User } from "@prisma/client";
-import { IGymsRepository } from "../gyms-repository";
+import { Gym, Prisma } from "@prisma/client";
+import { FindManyNearbyParams, IGymsRepository } from "../gyms-repository";
 import { randomUUID } from "crypto";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 export class InMemoryGymsRepository implements IGymsRepository {
   public items: Gym[] = [];
@@ -34,5 +35,22 @@ export class InMemoryGymsRepository implements IGymsRepository {
     return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20);
+  }
+
+  async findManyNearby(params: FindManyNearbyParams): Promise<Gym[]> {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        {
+          latitude: params.latitude,
+          longitude: params.longitude,
+        },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+
+      return distance < 10; // 10 kilometers
+    });
   }
 }
---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { FetchNearbyGyms } from "./fetch-nearby-gyms";
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: FetchNearbyGyms;
+
+describe("Fetch nearby gyms use case", () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new FetchNearbyGyms(gymsRepository);
+  });
+
+  it("should be able to fetch nearby gyms", async () => {
+    await gymsRepository.create({
+      title: "Near Gym",
+      description: null,
+      phone: null,
+      latitude: -22.4187356,
+      longitude: -45.4770245,
+    });
+
+    await gymsRepository.create({
+      title: "Far Gym",
+      description: null,
+      phone: null,
+      latitude: -22.3114915,
+      longitude: -45.362409,
+    });
+
+    const { gyms } = await sut.execute({
+      userLatitude: -22.4369982,
+      userLongitude: -45.5033868,
+    });
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([expect.objectContaining({ title: "Near Gym" })]);
+  });
+});
